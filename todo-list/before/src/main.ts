903398faf859aae82ed82419fa319f49d0a14b16
sel-mort <mortajisalem@gmail.com>
2024-11-03 19:55:32 +0100

    delete finished

--- todo-list/before/src/main.ts
+++ todo-list/before/src/main.ts
@@ -27,11 +27,13 @@ form.addEventListener('submit', (e: Event) => {
   input.value = "";
 });
 
-function createNewTodoNode({ name, completed }: Todo) {
+function createNewTodoNode({ id, name, completed }: Todo) {
   const template = document.querySelector('#template')! as HTMLTemplateElement;
   const cloneList = template.content.cloneNode(true) as HTMLLIElement;
   const labelText = cloneList.querySelector('.label-text')!;
   const labelInput = cloneList.querySelector<HTMLInputElement>('.label-input')!;
+  const li = cloneList.children[0];
+  li.id = id;
   labelText.textContent = name;
   labelInput.checked = completed;
 
@@ -43,8 +45,9 @@ list?.addEventListener('click', (e: Event) => {
   
   if (target.matches('.delete-btn')) {
     target.closest('.list-item')?.remove();
-    const todo = target.id;
-    deleteTodo(todoList, todo);
+    console.log(target.closest('.list-item'))
+    const todo = target.closest('.list-item')?.id;
+    deleteTodo(todoList, todo ?? '');
   }
 });
 
