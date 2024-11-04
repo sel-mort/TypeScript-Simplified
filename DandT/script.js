5695e70611156005daa1b0c18659d01c86a4f1e0
sel-mort <mortajisalem@gmail.com>
2024-11-04 20:23:41 +0100

    debounce and throttle

--- DandT/script.js
+++ DandT/script.js
@@ -5,4 +5,17 @@ const throttle = document.querySelector('#throttle');
 
 input.addEventListener('input', (e) => {
     defaultVar.textContent = e.target.value;
-})
+
+    debounceFunc(e, 1000);
+
+});
+
+function debounceFunc(e, delay) {
+    let debounceInterval;
+    return () => {
+        setTimeout(() => {
+            clearTimeout(debounceInterval);
+            debounce.textContent = e.target.value;
+        }, delay);
+    }
+}
